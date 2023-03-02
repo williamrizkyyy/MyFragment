dd5edd83884abaa2f5d7211d3209a7b2a8fa4ccf
williamrizkyyy <101534537+williamrizkyyy@users.noreply.github.com>
2023-03-02 13:56:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/myfragment/FirstFragment.kt
@@ -0,0 +1,66 @@
+package com.example.myfragment
+
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProviders
+import com.example.fragment.CommunicationViewModel
+import com.google.android.material.textfield.TextInputEditText
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var communicationViewModel: CommunicationViewModel? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+       communicationViewModel = ViewModelProviders.of(requireActivity()).get(CommunicationViewModel::class.java)
+        }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val nameEditText = view.findViewById<TextInputEditText>(R.id.textInputTextName)
+        nameEditText.addTextChangedListener(
+            object : TextWatcher{
+                override fun beforeTextChanged(charSequence: CharSequence?, i: Int, i1: Int, i2: Int) {
+                }
+
+                override fun onTextChanged(charSequence: CharSequence?, i: Int, i1: Int, i2: Int) {
+                   communicationViewModel!!.setName(charSequence.toString())
+                }
+
+                override fun afterTextChanged(editable: Editable?) {
+
+                }
+            }
+        )
+    }
+    companion object {
+        fun newInstance(): FirstFragment{
+            return FirstFragment()
+        }
+    }
+}
